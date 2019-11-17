c49f415cea3c2fad5734ba5b3285f64bcaea2856
Gergely Acs <gergely.acs89@gmail.com>
2019-11-17 20:21:22 +0100

    Created a simple resource object chasing algorithm.

---
+++ Assets/Scripts/Minion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour
+{
+    [SerializeField] private float speed = 200.0f;
+
+    private Vector2 targetPosition;
+    private readonly float distanceThreshold = 10.0f;
+
+    void Start()
+    {
+        FindRandomTargetLocation();
+    }
+
+    void Update()
+    {
+        SearchForResource();
+        MoveTowardTarget();
+    }
+
+    private void SearchForResource()
+    {
+        Resource[] resources = FindObjectsOfType<Resource>();
+        if (resources.Length == 0) return;
+
+        float minDistance = float.MaxValue;
+        foreach (Resource resource in resources)
+        {
+            float distance = Vector2.Distance(transform.position, resource.transform.position);
+            if (minDistance > distance)
+            {
+                minDistance = distance;
+                targetPosition = resource.transform.position;
+            }
+        }
+    }
+
+    private void MoveTowardTarget()
+    {
+        if (Vector2.Distance(targetPosition, transform.position) > distanceThreshold)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, step);
+        }
+        else
+        {
+            FindRandomTargetLocation();
+        }
+    }
+
+    private void FindRandomTargetLocation()
+    {
+        int xLim = Screen.currentResolution.width / 2;
+        int yLim = Screen.currentResolution.height / 2;
+        
+        targetPosition = new Vector2(UnityEngine.Random.Range(-xLim, xLim), UnityEngine.Random.Range(-yLim, yLim));
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject resource;
+    [SerializeField] float resourceSpawnTimerInSeconds = 3.0f;
+
+    GameObject resourceParent;
+    const string cResourceParentName = "Resources";
+
+    void Start()
+    {
+        CreateResourceParent();
+        StartCoroutine(SpawnResource());
+    }
+
+    private void CreateResourceParent()
+    {
+        resourceParent = GameObject.Find(cResourceParentName);
+        if (!resourceParent)
+        {
+            resourceParent = new GameObject(cResourceParentName);
+        }
+    }
+
+    IEnumerator SpawnResource()
+    {
+        while (true)
+        {
+            int xLim = Screen.currentResolution.width / 2;
+            int yLim = Screen.currentResolution.height / 2;
+            Vector3 targetPosition = new Vector3(Random.Range(-xLim, xLim), Random.Range(-yLim, yLim), 0);
+
+            GameObject newResource = Instantiate(resource, targetPosition, Quaternion.identity);
+            newResource.transform.parent = resourceParent.transform;
+            yield return new WaitForSeconds(resourceSpawnTimerInSeconds);
+        }
+    }
+}
